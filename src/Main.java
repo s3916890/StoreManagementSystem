dff03a8611cfd1e9fa70c4a255a413a5f94552a6
karma.2710 <95325806+s3916890@users.noreply.github.com>
2022-08-11 00:14:25 +0700

    commit file main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
+import controller.Order;
+import controller.Product;
+import controller.Users;
 import view.Menu;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Menu menu = new Menu();
-        menu.view();
-//        Users user = new Users(1, "40.karma", "0909150472");
-//        Product product = new Product(1, "laptop", "Macbook Pro 13 inch Retina 2015 MLL42 Core i5 6360U", "silver",
-//                Long.parseLong("17900000"));
+//        menu.view();
+
+        Users user = new Users(1, "40.karma", "0909150472");
+        Product product = new Product(1, "laptop", "Macbook Pro 13 inch 2016 Four Thunderbolt 3 Port MLH12 TouchBar Core i5 6267U", "silver",
+                Long.parseLong("17900000"));
+        Order order = new Order();
+
+//        order.createNewOrder(user, product);
+        order.searchOrder();
     }
 
 }
