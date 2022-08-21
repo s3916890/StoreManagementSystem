8fb5b8cd5903e745f7d66f57d56122f301ad0f9e
karma.2710 <95325806+s3916890@users.noreply.github.com>
2022-08-21 16:02:24 +0700

    remove abundant
    remove abundant code in main

--- src/Main.java
+++ src/Main.java
@@ -20,23 +20,5 @@ public class Main {
 
 //        menu.view();
 //        adminMenu.view();
-
-//        ArrayList<String[]> productOptions = Read.readAllLine( "products.txt");
-//
-//        for(String[] obj: productOptions){
-//            System.out.println(Arrays.toString(obj));
-//        }
-//        System.out.println(productOptions.get(12)[0]);
-//        System.out.println(productOptions.get(12)[1]);
-//        System.out.println(productOptions.get(12)[2]);
-//        System.out.println(productOptions.get(12)[3]);
-//        System.out.println(productOptions.get(12)[4]);
-
-//        Customer customer = new Customer(3, "40.karma", "0909150472");
-//        Product product = new Product(1, "laptop", "Macbook Pro 13 inch Retina 2015", "silver", Long.parseLong("25000000"));
-//
-//        Order order = new Order();
-//
-//        order.createNewOrder(customer, product);
     }
 }
